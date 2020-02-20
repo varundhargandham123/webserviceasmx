1c384a748bb86b4bd9cc4f963c927c7e775f9790
Varundhar_Gandham <Varundhar_Gandham@EPINHYDW013A>
2020-02-20 14:26:46 +0530

    Add project files.

---
+++ webserviceasmx/WebForm1.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="webserviceasmx.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Button ID="add" runat="server" Text="Add" OnClick="add_Click" />
+&nbsp;&nbsp;
+            <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+        </div>
+        <asp:Button ID="sub" runat="server" Text="Sub" OnClick="sub_Click" />
+&nbsp;&nbsp;
+        <asp:Label ID="Label2" runat="server" Text="Label"></asp:Label>
+    </form>
+</body>
+</html>
---
+++ webserviceasmx/WebForm1.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace webserviceasmx
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void add_Click(object sender, EventArgs e)
+        {
+            WebService1 webservice1 = new WebService1();
+            Label1.Text = webservice1.Add(5, 10).ToString();
+        }
+        protected void sub_Click(object sender, EventArgs e)
+        {
+            WebService1 webservice1 = new WebService1();
+            Label2.Text = webservice1.Sub(10, 5).ToString();
+        }
+    }
+}
